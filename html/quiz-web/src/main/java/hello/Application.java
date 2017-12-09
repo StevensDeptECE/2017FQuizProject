d1d7907911ef2e1238698c62d34f9a57d9782b60
zhaoyi <genius.zhaoyi@gmail.com>
2017-12-09 11:53:14 -0500

    html start

---
+++ html/quiz-web/src/main/java/hello/Application.java
@@ -0,0 +1,22 @@
+package hello;
+
+import org.apache.catalina.connector.Connector;
+import org.apache.catalina.Context;
+
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
