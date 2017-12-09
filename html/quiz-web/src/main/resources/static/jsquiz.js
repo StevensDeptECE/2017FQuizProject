f14c0f5ad9e682ca639651b816782639d51fd99c
zhaoyi <genius.zhaoyi@gmail.com>
2017-12-09 13:14:34 -0500

    use my own bean class

--- html/quiz-web/src/main/resources/static/jsquiz.js
+++ html/quiz-web/src/main/resources/static/jsquiz.js
@@ -1,14 +1,13 @@
-function copyText()
-{
-var radios = document.getElementsByClassName("radios");
-var scores = 0;
-for(var i=0;i<radios.length;i++){
-	var fruit = document.getElementsByName("Fruit" + (i+1));
-	for(var j=0;j<fruit.length;j++){
-		if(fruit[j].checked){
-			scores += fruit[j].value * 1;
+function copyText() {
+	var radios = document.getElementsByClassName("radios");
+	var scores = 0;
+	for(var i=0;i<radios.length;i++){
+		var fruit = document.getElementsByName("Fruit" + (i+1));
+		for(var j=0;j<fruit.length;j++){
+			if(fruit[j].checked){
+				scores += fruit[j].value * 1;
+			}
 		}
 	}
-}
-document.getElementById("score").value=scores;
+	document.getElementById("score").value=scores;
 }
